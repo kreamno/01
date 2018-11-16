f5a1dbe5d10378e3a3871751faf40d54e0d16f07
yujeong <kreamno@gmail.com>
2018-11-17 01:22:16 +0900

    initiate_bingo1

--- main.c
+++ main.c
@@ -2,32 +2,49 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define bingo_size 100
+#define bingo_size 25
 
-int main()
+int main()											//빙고판을 설정할 때 빙고의 사이즈와 빙고판에서 가로축의 크기를 확인하여 바꾸어야 한다. 
 {
 	int bingo_num;
 	int i;
 	int BINGO[bingo_size];
+	int BINGO0[bingo_size];
  
-	printf("Enter the number of random numbers (<=100) : ");	// bingo판의 크기를 설정한다. 
+	printf("Enter the number of random numbers (<=25) : ");		// bingo판의 크기를 설정한다. 
 	scanf("%d", &bingo_num);
 	
 	srand( (unsigned) time(NULL) );	
 	
-	for (i = 0; i < bingo_num; i++)
+	for (i = 0; i < bingo_num; i++)				//user's bingo
 		{ 
-		BINGO[i] = rand() % 100;	
+		BINGO[i] = rand() % 25;	
 		}
-
-	for (i = 0; i < bingo_num; i++)
-	{
-	if (i % 5 == 0)				//빙고판의 가로축에 있는 숫자의 개수를 5로 설정 
-	printf("\n");
-	printf("%5d", BINGO[i]);
 	
-	}
-	printf("\n");
+		printf("your bingo is:\n");			
+		for (i = 0; i < bingo_num; i++)
+		{
+		if (i % 5 == 0)
+		printf("\n");							//빙고판의 가로축에 있는 숫자의 개수를 5로 설정 
+		printf("%5d", BINGO[i]);
+		}
+		printf("\n");
+	
+	
+	for (i = 0; i < bingo_num; i++)				//computer's bingo
+		{ 
+		BINGO0[i] = rand() % 25;	
+		}
+	
+		printf("computer's bingo is:\n");	
+		for (i = 0; i < bingo_num; i++)
+		{
+		if (i % 5 == 0)
+		printf("\n");							//빙고판의 가로축에 있는 숫자의 개수를 5로 설정 
+		printf("%5d", BINGO0[i]);
+		}
+		printf("\n");
+	
 	
 	return 0;
 
